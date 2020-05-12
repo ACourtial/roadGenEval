0096a7fbd9a220991fb9256cb64167c5c02a0c6a
ACourtial <azelle.courtial@gmail.com>
2020-05-12 09:16:13 +0200

    pages tests pretes

--- main.js
+++ main.js
@@ -1,20 +1,6 @@
-const imgs= document.querySelectorAll('.img');
-const rks= document.querySelectorAll('.empty_square');
-const img_row=document.querySelectorAll('.image_row');
-let draggedItem=null;
-
 const but=document.querySelectorAll('.button');
-const cancel=document.getElementById('cancel');
-cancel.addEventListener('click',function(e){
-  for (let j=0; j<rks.length; j++){
-    var rk=rks[j];
-    var aimg= rk.querySelectorAll('.img');
-    for (var i = 0; i <aimg.length; i++){
-    rk.removeChild(aimg[0]);
-    img_row[0].append(aimg[0]);};
-  };
-});
-if ( typeof(document.getElementById('submitbutton')) == 'undefined' ){
+
+
 document.addEventListener('click', function (){
     console.log('click');
     var f = document.querySelectorAll('.form');
@@ -33,54 +19,4 @@ document.addEventListener('click', function (){
     if (cansubmit) {
         document.getElementById('submitbutton').disabled = false;
     }
-    else {
-        document.getElementById('submitbutton').disabled = 'disabled';
-    }
-});};
-
-const previouspos=[img_row[0],img_row[0],img_row[0],img_row[0]];
-console.log(previouspos)
-for (let i=0; i<imgs.length; i++){
-  const img=imgs[i];
-  var initpos=img_row[0];
-  img.addEventListener('dragstart',function(e){
-
-    draggedItem=this;
-    initpos=this.parentNode
-
-  });
-  img.addEventListener('dragend',function(){
-
-    setTimeout(function(){
-           draggedItem=null;
-     },0);
-  });
-
-  for (let j=0; j<rks.length; j++){
-    const rk=rks[j];
-    rk.addEventListener('dragover',function(e){
-      e.preventDefault();
-    });
-    rk.addEventListener('dragenter',function(e){
-      e.preventDefault();
-    });
-    rk.addEventListener('drop',function(e){
-
-      if (this.childNodes.length>1){
-          aimg= this.querySelectorAll('.img');
-          if (aimg[0].id==i){var k=1}else{var k=0};
-          rk.removeChild(aimg[k]);
-          initpos.append(aimg[k]);
-          this.append(draggedItem);
-          previouspos[i]=rk;
-          previouspos[aimg[k].id] =initpos;
-      }
-      else{
-        this.append(draggedItem);
-        previouspos[i]=rk;
-      }
-
-
-    });
-  }
-}
+});
---
+++ res.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+  <link href="style.css" rel="stylesheet">
+  <title>Rank my images</title>
+
+</head>
+
+<body>
+<?php
+require_once 'C:/Users/azelle/Documents/google-api-php-client-2.4.1/vendor/autoload.php';
+$client_id= '844120494739-v5j9pibracee8u2sb5m3n71d4p5g91it.apps.googleusercontent.com';
+$client_secret= 'vaCbLzIcI6qvUYA0C2eY56H3';
+function getClient()
+{
+    $client = new Google_Client();
+    $client->setApplicationName('Google Drive API PHP Quickstart');
+    $client->setScopes(Google_Service_Drive::DRIVE_METADATA_READONLY);
+    $client->setAuthConfig('credentials.json');
+    $client->setAccessType('offline');
+    $client->setPrompt('select_account consent');
+
+    // Load previously authorized token from a file, if it exists.
+    // The file token.json stores the user's access and refresh tokens, and is
+    // created automatically when the authorization flow completes for the first
+    // time.
+    $tokenPath = 'token.json';
+    if (file_exists($tokenPath)) {
+        $accessToken = json_decode(file_get_contents($tokenPath), true);
+        $client->setAccessToken($accessToken);
+    }
+
+    // If there is no previous token or it's expired.
+    if ($client->isAccessTokenExpired()) {
+        // Refresh the token if possible, else fetch a new one.
+        if ($client->getRefreshToken()) {
+            $client->fetchAccessTokenWithRefreshToken($client->getRefreshToken());
+        } else {
+            // Request authorization from the user.
+            $authUrl = $client->createAuthUrl();
+            printf("Open the following link in your browser:\n%s\n", $authUrl);
+            print 'Enter verification code: ';
+            $authCode = trim(fgets(STDIN));
+
+            // Exchange authorization code for an access token.
+            $accessToken = $client->fetchAccessTokenWithAuthCode($authCode);
+            $client->setAccessToken($accessToken);
+
+            // Check to see if there was an error.
+            if (array_key_exists('error', $accessToken)) {
+                throw new Exception(join(', ', $accessToken));
+            }
+        }
+        // Save the token to a file.
+        if (!file_exists(dirname($tokenPath))) {
+            mkdir(dirname($tokenPath), 0700, true);
+        }
+        file_put_contents($tokenPath, json_encode($client->getAccessToken()));
+    }
+    return $client;
+}
+
+
+// Get the API client and construct the service object.
+$client = getClient();
+$service = new Google_Service_Drive($client);
+
+// Print the names and IDs for up to 10 files.
+$optParams = array(
+  'pageSize' => 10,
+  'fields' => 'nextPageToken, files(id, name)'
+);
+$results = $service->files->listFiles($optParams);
+
+if (count($results->getFiles()) == 0) {
+    print "No files found.\n";
+} else {
+    print "Files:\n";
+    foreach ($results->getFiles() as $file) {
+        printf("%s (%s)\n", $file->getName(), $file->getId());
+    }
+}
+echo'douze'; ?>
+
+</body>
+
+</html>
