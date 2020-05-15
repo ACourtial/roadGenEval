70db1f5e8b5e8c0aa39779c5a8b8e92c23f32d6e
ACourtial <35691368+ACourtial@users.noreply.github.com>
2020-05-15 13:09:03 +0200

    Update firstpage.js

--- firstpage.js
+++ firstpage.js
@@ -1,7 +1,7 @@
 var id =257;
 var itime=Date.now();
 var page=0;
-var url = "ACourtial/data/first_test.txt";
+var url = "ACourtial/roadGenEval/data/first_test.txt";
 
 window.addEventListener('beforeunload',function (event){
   // Cancel the event as stated by the standard.
