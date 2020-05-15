12aaabae104acce4e5fdf0d4bc7ab8e97c35ce94
ACourtial <azelle.courtial@gmail.com>
2020-05-15 14:10:28 +0200

    zgke

--- firstpage.js
+++ firstpage.js
@@ -1,7 +1,7 @@
 var id =257;
 var itime=Date.now();
 var page=0;
-var url = "roadGenEval/data/first_test.csv";
+var url = "https://acourtial.github.io/roadGenEval/data/first_test.csv";
 
 window.addEventListener('beforeunload',function (event){
   // Cancel the event as stated by the standard.
